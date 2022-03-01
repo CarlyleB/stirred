dd08d5a887a2865bb2880f2160123ede853101be
Carly Burroughs <burroughs.carlyle@gmail.com>
2022-03-01 00:28:15 -0500

    Added /glasses and /categories endpoints

--- api/index.ts
+++ api/index.ts
@@ -18,13 +18,38 @@ app.listen(port, () => {
 const Pool = require('pg').Pool
 const pool = new Pool()
 
-const getDrinks = (request, response) => {
-    pool.query('SELECT * FROM drinks ORDER BY name DESC', (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).json(results.rows)
+const getDrinks = (_req, res) => {
+    pool.query('SELECT * FROM drinks ORDER BY name ASC', (error, results) => {
+        if (error) throw error
+        res.status(200).json(results.rows)
+    })
+}
+
+const getIngredients = (_req, res) => {
+    pool.query('SELECT * FROM ingredients ORDER BY name ASC', (error, results) => {
+        if (error) throw error
+        res.status(200).json(results.rows)
+    })
+}
+
+const getGlasses = (_req, res) => {
+    pool.query('SELECT * FROM glasses ORDER BY name ASC', (error, results) => {
+        if (error) throw error
+        res.status(200).json(results.rows)
+    })
+}
+
+const getCategories = (_req, res) => {
+    pool.query('SELECT * FROM categories ORDER BY name ASC', (error, results) => {
+        if (error) throw error
+        res.status(200).json(results.rows)
     })
 }
 
 app.get('/drinks', getDrinks);
+
+app.get('/ingredients', getIngredients);
+
+app.get('/glasses', getGlasses);
+
+app.get('/categories', getCategories);
